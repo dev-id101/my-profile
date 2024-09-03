3751a5bab12500bab748e1547695a45cc7c19bb6
Idorenyin <iniunamid@gmail.com>
2024-09-03 18:37:59 +0000

    test

--- lib/qr_code_page.dart
+++ lib/qr_code_page.dart
@@ -26,57 +26,56 @@ class QrCodePage extends StatelessWidget {
         title: const Text('Share Profile'),
       ),
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(16)),
+            padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                QrImageView(
+                  data: 'This QR code has an embedded image as well',
+                  version: QrVersions.auto,
+                  size: 200,
+                  gapless: false,
+                ),
+                const Text(
+                  '@man.like.iniunam',
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50),
+            child: Container(
               decoration: BoxDecoration(
                   color: Colors.grey.shade300,
                   borderRadius: BorderRadius.circular(16)),
-              padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  QrImageView(
-                    data: 'This QR code has an embedded image as well',
-                    version: QrVersions.auto,
-                    size: 200,
-                    gapless: false,
-                  ),
-                  const Text(
-                    '@man.like.iniunam',
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 25,
-                        fontWeight: FontWeight.w500),
-                  ),
+                  shareRow(
+                      icon: const Icon(Icons.share), text: 'Share profile'),
+                  shareRow(icon: const Icon(Icons.copy), text: 'Copy link'),
+                  shareRow(icon: const Icon(Icons.download), text: 'Download')
                 ],
               ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50),
-              child: Container(
-                decoration: BoxDecoration(
-                    color: Colors.grey.shade300,
-                    borderRadius: BorderRadius.circular(16)),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    shareRow(icon: const Icon(Icons.share), text: 'Share profile'),
-                    shareRow(icon: const Icon(Icons.copy), text: 'Copy link'),
-                    shareRow(icon: const Icon(Icons.download), text: 'Download')
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+        ],
+      )),
     );
   }
 
@@ -92,7 +91,7 @@ class QrCodePage extends StatelessWidget {
               color: Colors.grey.shade300),
           child: icon,
         ),
-         Text(
+        Text(
           text,
           style: const TextStyle(
               color: Colors.black, fontSize: 12, fontWeight: FontWeight.w500),
